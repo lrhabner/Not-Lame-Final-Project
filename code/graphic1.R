2e2067c2d72d0fb358f1b6ae839e3fffc2509f04
Dawn Thompson <dat03@uw.edu>
2018-12-04 10:14:15 -0800

    Add summary variables

--- code/graphic1.R
+++ code/graphic1.R
@@ -1,9 +1,17 @@
 # Can the level of engagement with players' Twitter accounts predict 
 # their team's audience turnout?
 
+library(dplyr)
 library(googleVis)
 
 dataset <- read.csv("../data/dataset.csv", stringsAsFactors = FALSE)
+
+highest_audience <- filter(dataset, AVG_AUDIENCE == max(AVG_AUDIENCE))
+lowest_audience <- filter(dataset, AVG_AUDIENCE == min(AVG_AUDIENCE))
+highest_retweets <- filter(dataset, TWITTER_RETWEET_COUNT == max(TWITTER_RETWEET_COUNT))
+lowest_retweets <- filter(dataset, TWITTER_RETWEET_COUNT == min(TWITTER_RETWEET_COUNT))
+highest_favorites <- filter(dataset, TWITTER_FAVORITE_COUNT == max(TWITTER_FAVORITE_COUNT))
+lowest_favorites <- filter(dataset, TWITTER_FAVORITE_COUNT == min(TWITTER_FAVORITE_COUNT))
 
 # change the behavior of plot.gvis so that only the chart component of the HTML file is written into the output file
 op <- options(gvis.plot.tag='chart')
