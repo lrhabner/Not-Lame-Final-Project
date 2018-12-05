fb5af4329572327c777bc690be8ccdb6863a0b18
Dawn Thompson <dat03@uw.edu>
2018-12-05 07:12:16 -0800

    Remove stat calculations

--- code/graphic1.R
+++ code/graphic1.R
@@ -6,13 +6,6 @@ library(googleVis)
 
 dataset <- read.csv("../data/dataset.csv", stringsAsFactors = FALSE)
 
-highest_audience <- filter(dataset, AVG_AUDIENCE == max(AVG_AUDIENCE))
-lowest_audience <- filter(dataset, AVG_AUDIENCE == min(AVG_AUDIENCE))
-highest_retweets <- filter(dataset, TWITTER_RETWEET_COUNT == max(TWITTER_RETWEET_COUNT))
-lowest_retweets <- filter(dataset, TWITTER_RETWEET_COUNT == min(TWITTER_RETWEET_COUNT))
-highest_favorites <- filter(dataset, TWITTER_FAVORITE_COUNT == max(TWITTER_FAVORITE_COUNT))
-lowest_favorites <- filter(dataset, TWITTER_FAVORITE_COUNT == min(TWITTER_FAVORITE_COUNT))
-
 colnames(dataset) <- c('X', 'Team', 'Number of Players', 'Average Audience', 'Twitter Favorite Count', 'Twitter Retweet Count')
 
 # change the behavior of plot.gvis so that only the chart component of the HTML file is written into the output file
