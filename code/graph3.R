cde45b412da5a00ceee2ef7841c6eb9316ae1cce
Logan Habner <lrhabner@uw.edu>
2018-12-04 21:49:32 -0800

    Changed graphic3 to gvisBubbleChart. Deleted unnecessary code.

--- code/graph3.R
+++ code/graph3.R
@@ -4,21 +4,21 @@ library(googleVis)
 unprocessedData <- data.table::fread('../data/nba_2016_2017_100.csv')
 
 data <- unprocessedData %>%
-        select(AGE, SALARY_MILLIONS, TWITTER_FOLLOWER_COUNT_MILLIONS) %>%
-        filter(TWITTER_FOLLOWER_COUNT_MILLIONS > 0) %>%
-        group_by(TWITTER_FOLLOWER_COUNT_MILLIONS) %>%
-        summarize(AGE = mean(AGE), SALARY_MILLIONS = mean(SALARY_MILLIONS)) %>%
-        arrange(TWITTER_FOLLOWER_COUNT_MILLIONS)
+        select(TWITTER_FOLLOWER_COUNT_MILLIONS, AGE, SALARY_MILLIONS, PLAYER_NAME) %>%
+        filter(TWITTER_FOLLOWER_COUNT_MILLIONS > 0)
 
 op <- options(gvis.plot.tag='chart')
 
-graphic3 <- gvisLineChart(data, xvar= "TWITTER_FOLLOWER_COUNT_MILLIONS", c("SALARY_MILLIONS","AGE"),
+graphic3 <- gvisBubbleChart(data, idvar = "PLAYER_NAME", 
+                          xvar = "TWITTER_FOLLOWER_COUNT_MILLIONS", yvar = "SALARY_MILLIONS",
+                          colorvar = "SALARY_MILLIONS", sizevar = "AGE",
                           options=list(height="800px", width="1000px",
                                        explorer="{actions:['dragToZoom', 'rightClickToReset']}",
                                        explorer="{maxZoomIn:.1}",
                                        series="[{targetAxisIndex: 0}, {targetAxisIndex:1}]",
                                        vAxes="[{title:'Salary'}, {title:'Age'}]",
                                        hAxis='{title:"Twitter Followers"}',
+                                       bubble="{textStyle:{color: 'none'}}",
                                        title="Correlation Between Twitter Followers, Age, and Salary (2017)"))
 
 createGraphic3 <- function() {plot(graphic3)}
