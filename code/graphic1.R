88e5dd0e5a752e047470e0134373eaa92e09e7c8
Logan Habner <lrhabner@uw.edu>
2018-12-05 02:19:52 -0800

    Removed width parameter so that graphic3 would match up with graphic1.

--- code/graphic1.R
+++ code/graphic1.R
@@ -13,12 +13,14 @@ lowest_retweets <- filter(dataset, TWITTER_RETWEET_COUNT == min(TWITTER_RETWEET_
 highest_favorites <- filter(dataset, TWITTER_FAVORITE_COUNT == max(TWITTER_FAVORITE_COUNT))
 lowest_favorites <- filter(dataset, TWITTER_FAVORITE_COUNT == min(TWITTER_FAVORITE_COUNT))
 
+colnames(dataset) <- c('X', 'Team', 'Number of Players', 'Average Audience', 'Twitter Favorite Count', 'Twitter Retweet Count')
+
 # change the behavior of plot.gvis so that only the chart component of the HTML file is written into the output file
 op <- options(gvis.plot.tag='chart')
 
 graphic1 <- gvisBubbleChart(dataset, idvar="Team", 
-                            xvar="TWITTER_RETWEET_COUNT", yvar="TWITTER_FAVORITE_COUNT",
-                            colorvar="NUM_PLAYERS", sizevar="AVG_AUDIENCE",
+                            xvar="Twitter Retweet Count", yvar="Twitter Favorite Count",
+                            colorvar="Number of Players", sizevar="Average Audience",
                             options=list(height="800px", 
                                          explorer="{actions:['dragToZoom', 'rightClickToReset']}",
                                          explorer="{maxZoomIn:.1}",
